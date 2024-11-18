bab6242f8a8cafd388b7d0968ba0c884b0c8a72e
김정섭 <jaydenkim850802@gmail.com>
2024-11-18 12:01:51 +0900

    Sequelize 적용

--- data/auth.js
+++ data/auth.js
@@ -1,17 +1,66 @@
-import { db } from '../db/database.js'
+import SQ from 'sequelize'
+import { sequelize } from '../db/database.js'
+// import { db } from '../db/database.js' // 필요없어짐
 
-export async function createUser(user) {   // user로 묶음???
-    const { username, password, name, email, url } = user
-    return db.execute('INSERT INTO users (username, password, name, email, url) VALUES (?, ?, ?, ?, ?)',
-        [username, password, name, email, url]) .then((result) => result[0].insertId)
-}
+//테이블도 여기서 객체로 만들어서 함
+const DataTypes = SQ.DataTypes
+
+//테이블 생성 .define('테이블 이름, {테이블 구성})
+export const User = sequelize.define(
+    'user', 
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            allowNull: false,
+            primaryKey: true
+        },
+        username: {
+            type: DataTypes.STRING(50),
+            allowNull: false
+        },
+        password: {
+            type: DataTypes.STRING(500),
+            allowNull: false
+        },
+        name: {
+            type: DataTypes.STRING(20),
+            allowNull: false
+        },
+        email: {
+            type: DataTypes.STRING(50),
+            allowNull: false
+        },
+        url: DataTypes.TEXT
+    },
+    { timestamps: false }  //creatAt 자동으로 안쓰게 만들어주는 것
+)
 
 export async function findByUsername(username) {
-    return db.execute('SELECT * FROM users WHERE username=?', [username])
-        .then((result) => result[0][0])
+    return User.findOne({where: { username }})
 }
 
 export async function findById(id) {
-    return db.execute('SELECT * FROM users WHERE id=?', [id])
-        .then((result) => result[0][0])
+    return User.findByPk(id)    
 }
+
+export async function createUser(user) {
+        return User.create(user).then((data) => data.dataValues.id)
+}
+
+// sequelize 하면서 다 필요없어짐
+// export async function createUser(user) {   // user로 묶음???
+//     const { username, password, name, email, url } = user
+//     return db.execute('INSERT INTO users (username, password, name, email, url) VALUES (?, ?, ?, ?, ?)',
+//         [username, password, name, email, url]) .then((result) => result[0].insertId)
+// }
+
+// export async function findByUsername(username) {
+//     return db.execute('SELECT * FROM users WHERE username=?', [username])
+//         .then((result) => result[0][0])
+// }
+
+// export async function findById(id) {
+//     return db.execute('SELECT * FROM users WHERE id=?', [id])
+//         .then((result) => result[0][0])
+// }
--- db/database.js
+++ db/database.js
@@ -1,11 +1,18 @@
-import mysql from 'mysql2'
-import { config } from '../config.js';
+import { config } from '../config.js'
+import SQ from 'sequelize'
 
-const pool = mysql.createPool({
-    host: config.db.host,
-    user: config.db.user,
-    database: config.db.database,
-    password: config.db.password
+const { host, user, database, password } = config.db
+export const sequelize = new SQ.Sequelize(database, user, password, {   // 초록색 = class 표시
+    host,
+    dialect: 'mysql',
+    logging: false
 })
 
-export const db = pool.promise()
+// const pool = mysql.createPool({   // 이제 안씀
+//     host: config.db.host,
+//     user: config.db.user,
+//     database: config.db.database,
+//     password: config.db.password
+// })
+
+// export const db = pool.promise()
